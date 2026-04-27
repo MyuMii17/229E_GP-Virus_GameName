e4d3214c3ad411a2c27037774e47d71f2fdefbb9
Ratsarun Thipdumrong <mewgamer17@gmail.com>
2026-04-27 18:30:23 +0700

    EnemySpawn For Test System

---
+++ Assets/MewAssest/Script/SpawnPoint/SpawnManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject enemyPrefeb;
+    private EnemyController enemySet;
+    private int enemyCount;
+    public EnemyController enemyrset;
+    public bool isHasEnemy;
+    void Awake()
+    {
+        enemyCount = 0;
+        spawnPoint = transform.GetChild(0).transform;
+    }
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.TryGetComponent(out EnemyController enemy))
+        {
+            enemyrset = enemy;
+        }
+    }
+    void Update()
+    {
+        if(enemyrset == null && enemyCount <= 0)
+        {
+            enemyCount++;
+            StartCoroutine(spawnEnemy());
+        }
+    }
+    IEnumerator spawnEnemy()
+    {
+        Instantiate(enemyPrefeb,spawnPoint.position,Quaternion.identity);
+        yield return new WaitForSeconds(1);
+        enemyCount = 0;
+    }
+}
