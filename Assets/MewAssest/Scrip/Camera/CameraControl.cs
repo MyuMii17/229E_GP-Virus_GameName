c557ab973b1ccd1f37f0fd21da76674f1c5daed0
patbadin-wq <patbadin54485448@gmail.com>
2026-05-01 19:22:39 +0700

    FixGameplay+shit

--- Assets/MewAssest/Scrip/Camera/CameraControl.cs
+++ Assets/MewAssest/Scrip/Camera/CameraControl.cs
@@ -19,9 +19,9 @@ public class CameraControl : MonoBehaviour
     {
         transform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, -10f);
 
-        if(transform.position.y < 1f)
+        if(transform.position.y < -28.5f)
         {
-            transform.position = new Vector3(playerTransform.position.x, 1f, -10f);
+            transform.position = new Vector3(playerTransform.position.x, -28.5f, -10f);
         }
     }
 }
